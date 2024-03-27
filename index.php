6547cb919217dca1ce6e7ae52e20dc6f1c1c4d71
arizkinewbie <arizkijitsu@gmail.com>
2024-03-27 11:17:44 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>:: Mudik Gratis Kementerian Perhubungan ::</title>
+  <meta name="csrf-param" content="_csrf-frontend">
+  <meta name="csrf-token" content="ozW5Pa9-YtzTl4zvo4Rxy8Gra65KJv4mv1znWVT6JHzPefJa7EtPtLbjtbvmsBOHpcYhwx5-tUrHJtQ2B7BhOw==">
+  <meta content="Mudik Gratis Kementerian Perhubungan adalah program yang diselenggarakan oleh Kementerian Perhubungan Republik Indonesia untuk memberikan akses mudik gratis kepada masyarakat yang ingin pulang kampung pada saat hari raya atau perayaan tertentu." name="description">
+  <meta content="Mudik Gratis Kementerian Perhubungan
+    Program Mudik Gratis Kemenhub
+    Syarat Mudik Gratis Kemenhub
+    Pendaftaran Mudik Gratis Kemenhub
+    Rute Mudik Gratis Kemenhub
+    Jadwal Mudik Gratis Kemenhub
+    Transportasi Mudik Gratis Kemenhub
+    Bantuan Mudik Gratis Kemenhub
+    Mudik Aman Mudik Nyaman
+    Pelaksanaan Mudik Gratis Kemenhub
+    Pengalaman Mudik Gratis Kemenhub
+    Manfaat Mudik Gratis Kemenhub
+    Informasi Mudik Gratis Kemenhub
+    Kontak Mudik Gratis Kemenhub
+    Berita Mudik Gratis Kemenhub" name="keywords">
+  <!-- Favicons -->
+  <link href="img/favicon.ico" rel="icon">
+  <script type="application/ld+json">
+    {
+      "@context": "https://schema.org/",
+      "@type": "WebSite",
+      "name": "Mudik Gratis",
+      "url": "https://mudikgratis.dephub.go.id"
+
+    }
+  </script>
+
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@kemenhub151">
+  <meta name="twitter:creator" content="@kemenhub151">
+  <meta property="og:url" content="https://mudikgratis.dephub.go.id">
+  <meta property="og:title" content="Mudik Gratis Kementerian Perhubungan">
+  <meta property="og:description" content="Mudik Gratis Kementerian Perhubungan adalah program yang diselenggarakan oleh Kementerian Perhubungan Republik Indonesia untuk memberikan akses mudik gratis kepada masyarakat yang ingin pulang kampung pada saat hari raya atau perayaan tertentu.">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i&amp;display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://mudikgratis.dephub.go.id/css/style.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
+  <style>
+    select.form-control {
+      -webkit-appearance: menulist !important;
+      -moz-appearance: menulist !important;
+      -ms-appearance: menulist !important;
+      -o-appearance: menulist !important;
+      appearance: menulist !important;
+    }
+  </style>
+  <script type="text/javascript">
+    window.hasMobileFirstExtension = true;
+  </script>
+</head>
+
+<body data-aos-easing="ease-in-out" data-aos-duration="1000" data-aos-delay="0" data-new-gr-c-s-check-loaded="14.1163.0" data-gr-ext-installed="">
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent header-scrolled">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="logo">
+        <h6>
+          <a href="#" aria-label="Link Halaman Utama"><span>
+              <svg width="450px" height="50px">
+                <image alt="Logo Kemenhub" href="img/logo.png" x="2" y="6" style="height:50px; width:150px;"></image>
+              </svg>
+            </span>
+          </a>
+        </h6>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#">Beranda</a></li>
+          <li><a class="nav-link scrollto" href="#about">Syarat &amp; Ketentuan</a></li>
+        </ul>
+        <i class="ri-menu-line mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header>
+  <!-- ======= Header ======= -->
+  <!-- End Header -->
+
+  <section id="hero">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
+          <div data-aos="zoom-out" class="aos-init aos-animate">
+            <h1>Payo Balek Duson <span>Bersama SAFA!</span></h1>
+            <h2>MUDIK GRATIS BERSAMA SAFA 2024.</h2>
+            <div class="text-center text-lg-start"></div>
+          </div>
+        </div>
+        <div class="col-lg-4 order-1 order-lg-2 hero-img aos-init aos-animate" data-aos="zoom-out" data-aos-delay="300"></div>
+      </div>
+    </div>
+
+    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+      <defs>
+        <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+        </path>
+      </defs>
+      <g class="wave1">
+        <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255,.1)"></use>
+      </g>
+      <g class="wave2">
+        <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255,.2)"></use>
+      </g>
+      <g class="wave3">
+        <use xlink:href="#wave-path" x="50" y="9" fill="#fff"></use>
+      </g>
+    </svg>
+  </section>
+
+  <main id="main">
+    <section id="about" class="about">
+      <div class="container">
+        <div class="section-title aos-init aos-animate" data-aos="fade-up">
+          <h2>S y a r a t &amp;</h2>
+          <p>Ketentuan</p>
+        </div>
+        <div class="row">
+          <hr>
+          <h3 class="card-title text-danger">
+            <i class="ri-edit-2-fill"></i>
+            Syarat Mudik Bersama SAFA
+          </h3>
+          <hr>
+          <div class="alert alert-info">
+            <p class="text-center">Pendaftaran dimulai pada 13 Maret 2024 - 7 April 2024</p>
+          </div>
+          <ol>
+            <li class="py-2 text-justify">
+              Satu peserta harus memiliki KTP, Kartu Keluarga, SIM C, STNK
+            </li>
+            <li class="py-2 text-justify">
+              Tidak membawa barang dalam ukuran atau jumlah berlebihan, yang dapat mengganggu mobilitas dan stabilitas saat mengendarai motor.
+            </li>
+            <li class="py-2 text-justify">
+              Masing-masing peserta dan penumpang wajib membawa helm sebagai kelengkapan keselamatan wajib dalam berkendara.
+            </li>
+            <li class="py-2 text-justify">
+              <p>Verifikasi Calon Peserta Arus Mudik, mulai <b>15 Maret 2024</b>, setiap hari <b>pukul 09.00 s.d. 16.00 WIB</b> (termasuk hari libur dan tanggal merah) di:</p>
+              <ul>
+                <li> Terminal Penumpang Nusantara Pelabuhan Utama Tanjung Priok Jl. Panaitan No. 105, Tanjung Priok – Jakarta Utara</li>
+                <li> Kantor Kementrian Perhubungan RI, Jl. Medan Merdeka Barat No. 8 Jakarta Pusat (wajib menggunakan kemeja atau polo shirt dan menggunakan sepatu. Mengingat keterbatasan lahan parkir maka disarankan untuk tidak membawa kendaraan, atau memarkir kendaraan di tempat lain)</li>
+              </ul>
+            </li>
+
+            <li class="py-2 text-justify">
+              Verifikasi Calon Peserta Arus Balik (Hanya untuk Peserta Arus Balik Semarang ke Jakarta saja yang berdomisili di Semarang dan sekitarnya), mulai <b>22 Maret 2024</b>, (termasuk hari libur dan tanggal merah) di <b>Kantor KSOP Kelas I Tanjung Emas, Jl. Yos Sudarso No.30, Bandarharjo, Kec. Semarang Utara, Kota Semarang - Jawa Tengah </b>
+            </li>
+          </ol>
+          <div class="btn-group mb-5" role="group" aria-label="Basic example">
+          </div>
+          <!-- <div class="col-md-6 col-lg-6 col-xl-6 col-xxl-6 mb-4">
+            <hr>
+            <h3 class="card-title text-danger">
+              <i class="ri-edit-2-fill"></i>
+              Syarat Mudik Kereta Api
+            </h3>
+            <hr>
+            <div class="alert alert-info">
+              <p class="text-center">Pendaftaran dimulai pada 4 Maret 2024 - 18 April 2024</p>
+            </div>
+            <ol>
+              <li class="py-2 text-justify">
+                Satu peserta harus memiliki KTP, Kartu Keluarga, SIM C
+              </li>
+              <li class="py-2 text-justify"><b>WAJIB</b> melakukan verifikasi ke Posko, sesuai dengan waktu verifikasi yang telah dipilih pada saat mendaftar</li>
+              <li>
+                Motor dengan kapasitas mesin maksimal 200 cc
+              </li>
+              <li class="py-2 text-justify">
+                Bagi Peserta yang hanya mendaftarkan Sepeda Motor,
+                diwajibkan memiliki /menunjukkan bukti Mudik moda
+                transportasi lainnya
+              </li>
+              <li class="py-2 text-justify">Sepeda Motor
+                diserahkan H-1 atau
+                satu hari
+                sebelum tanggal keberangkatan Sepeda Motor</li>
+              <li class="py-2 text-justify">Tidak diperkenankan
+                menitipkan Helm
+                dan Kaca
+                Spion.</li>
+              <li class="py-2 text-justify">BBM wajib untuk
+                dikosongkan pada
+                saat penyerahan</li>
+            </ol>
+            <div class="btn-group mb-5" role="group" aria-label="Daftar Untuk Kereta Api">
+              <a target="_blank" href="https://motis.djka.dephub.go.id/" class="btn btn-danger" aria-label="Daftar Mudik Gratis Moda Kereta Api">
+                <i class="ri-subway-fill"></i> Daftar Untuk Kereta Api
+              </a>
+            </div>
+          </div> -->
+        </div>
+      </div>
+    </section><!-- End About Section -->
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright"> © Hak Cipta <strong><span>Pusdatin</span></strong>. Kementerian Perhubungan </div>
+      <p class="text-center mt-2" style="font-size:24px">
+        <a rel="nofollow" href="https://instagram.com/kemenhub151" target="_blank" aria-label="Link Instagram 151">
+          <i class="ri-instagram-line"></i>
+        </a>
+        <a rel="nofollow" href="https://facebook.com/kemenhub151/?hl=en" target="_blank" aria-label="Link Facebook 151">
+          <i class="ri-facebook-box-fill"></i>
+        </a>
+        <a rel="nofollow" href="https://twitter.com/kemenhub151" target="_blank" aria-label="Link Twitter 151">
+          <i class="ri-twitter-fill"></i>
+        </a>
+      </p>
+    </div>
+
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center active">
+    <i class="ri-arrow-up-line"></i>
+  </a>
+  <script src="js/jquery.js"></script>
+  <script src="js/yii.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
+  <script src="js/main.js"></script>
+
+</html>
